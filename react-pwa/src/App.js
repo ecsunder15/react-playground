9a5f95a25daf357d546281adf8578f28dd3f613a
Elise Sunderland <esunderland@appnexus.com>
2019-12-16 16:14:34 -0800

    add buttons to public API, add country dropdown

--- react-pwa/src/App.js
+++ react-pwa/src/App.js
@@ -1,48 +1,61 @@
 import React from 'react';
 import Button from '@material-ui/core/Button'
+import Locations from './Locations';
+import { makeStyles } from '@material-ui/core/styles';
 import logo from './xandr-logo.svg';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+		},
+	},
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className="App-body" alt="buttonGroup">
-        <Button
-          className="App-button"
-          variant="contained"
-          color="primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Started
-        </Button>
-        <Button
-          className="App-button"
-          variant="contained"
-          color="primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Started
-        </Button>
-        <Button
-          className="App-button"
-          variant="contained"
-          color="primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Started
-        </Button>
-      </div>
-    </div>
-  );
+	const classes = useStyles();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+			<div className="App-body" alt="buttonGroup">
+				<Locations />
+				<div className={classes.root}>
+					<div>What kind of set up are you looking to start?</div>
+					<Button
+						variant="contained"
+						color="primary"
+						href="https://wiki.xandr.com/display/api/Buy-Side+Services"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Advertiser
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						href="https://wiki.xandr.com/display/api/Sell-Side+Services"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Publisher
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						href="https://wiki.xandr.com/display/api/API+Getting+Started"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Ad Server
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ react-pwa/src/Locations.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import _ from 'lodash';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import {
+	CountryRegionData
+} from "react-country-region-selector";
+
+const useStyles = makeStyles(theme => ({
+	button: {
+		display: 'block',
+		marginTop: theme.spacing(2),
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+	},
+}));
+
+const countryOptions = _.map(CountryRegionData, country => {
+	return _.first(country);
+});
+
+export default function Locations() {
+	const classes = useStyles();
+	const [country, setCountry] = React.useState('');
+	const [open, setOpen] = React.useState(false);
+
+	const handleChange = event => {
+		setCountry(event.target.value);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	return(
+		<div>
+			<h2>Where you at?</h2>
+			<FormControl className={classes.formControl}>
+				<InputLabel id="controlled-open-select-label">Country</InputLabel>
+				<Select
+					labelId="controlled-open-select-label"
+					id="controlled-open-select"
+					open={open}
+					onClose={handleClose}
+					onOpen={handleOpen}
+					value={country}
+					onChange={handleChange}
+				>
+					<MenuItem value="">
+						<em>Select</em>
+					</MenuItem>
+					{_.map(countryOptions, (country, idx) => {
+						return (
+							<MenuItem key={idx} value={country}>{country}</MenuItem>
+						);
+					})}
+				</Select>
+			</FormControl>
+		</div>
+	)
+}
